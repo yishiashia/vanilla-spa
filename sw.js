5808d0a0621ad88b7d9dd07d9c4b0815e5655e7c
yishiashia <yishiashia@gmail.com>
2023-12-29 16:34:07 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -17,15 +17,16 @@ self.addEventListener("fetch", function (event) {
       // 如果快取中有資源的副本，則立即提供該資源
       if (response) {
         return response;
-      }
-      // 否則，發送網絡請求以獲取資源
-      return fetch(event.request).then(function (response2) {
-        // 獲取到最新的資源後，將其添加到快取中
-        caches.open("my-cache").then(function (cache) {
-          cache.put(event.request, response2.clone());
+      } else {
+        // 否則，發送網絡請求以獲取資源
+        return fetch(event.request).then(function (response2) {
+          // 獲取到最新的資源後，將其添加到快取中
+          caches.open("my-cache").then(function (cache) {
+            cache.put(event.request, response2.clone());
+          });
+          return response2;
         });
-        return response2;
-      });
+      }
     })
   );
 });
