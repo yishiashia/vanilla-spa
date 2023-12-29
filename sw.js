33c55eea5b8843a90b2b154f649083ecd7c465ef
yishiashia <yishiashia@gmail.com>
2023-12-29 16:41:23 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,6 +2,7 @@ self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open("my-cache").then(function (cache) {
       return cache.addAll([
+        "/vanilla-spa/",
         "/vanilla-spa/index.html",
         "/vanilla-spa/index.js",
         "/vanilla-spa/style.css",
@@ -15,12 +16,9 @@ self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
       // 如果快取中有資源的副本，則立即提供該資源
-      console.log("111", event.request, response);
       if (response) {
-        console.log("222");
         return response;
       } else {
-        console.log("333");
         // 否則，發送網絡請求以獲取資源
         return fetch(event.request).then(function (response2) {
           // 獲取到最新的資源後，將其添加到快取中
