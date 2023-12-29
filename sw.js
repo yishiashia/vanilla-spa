940d5dacf337f0870f225ed193fe460ac0250613
yishiashia <yishiashia@gmail.com>
2023-12-29 16:37:24 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -15,9 +15,12 @@ self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
       // 如果快取中有資源的副本，則立即提供該資源
+      console.log("111", event.request, response);
       if (response) {
+        console.log("222");
         return response;
       } else {
+        console.log("333");
         // 否則，發送網絡請求以獲取資源
         return fetch(event.request).then(function (response2) {
           // 獲取到最新的資源後，將其添加到快取中
