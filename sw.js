92fd7adb9714c8d81c37027692007eab97298c41
yishiashia <yishiashia@gmail.com>
2023-12-29 16:13:35 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open("my-cache").then(function (cache) {
+      return cache.addAll(["/vanilla-spa/index.html"]);
+    })
+  );
+});
+
 self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
